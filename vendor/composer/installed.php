7572ea29ace0bae9bdce8f1d4efdb49239853641
KAmchai Boosnri <kamchai_n@outlook.com>
2021-03-09 21:26:06 +0700

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bfd7b2e980304be3e5badfd6d38daca67ab2a0ae',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bfd7b2e980304be3e5badfd6d38daca67ab2a0ae',
+    ),
+  ),
+);
